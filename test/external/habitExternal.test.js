80d87c38f2ed7aa7e415ea3111ef528eb819abd6
guilhermesm <guilherme.serrate.maia@gmail.com>
2025-09-19 22:33:07 -0300

    Adicionando steps de pipeline para execução dos testes.

---
+++ test/external/habitExternal.test.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const { PassThrough } = require('supertest/lib/test');
+
+(async () => {
+    const chai = await import('chai');
+    expect = chai.expect;
+})();
+
+describe('Habit Endpoints', () => {});
