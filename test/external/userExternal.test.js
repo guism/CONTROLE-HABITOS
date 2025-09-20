91b5b33eb6ab18db285b6108396982aab215bb69
guilhermesm <guilherme.serrate.maia@gmail.com>
2025-09-19 22:11:33 -0300

    Adicionando ultimos testes do endpoint de usuários.

--- test/external/userExternal.test.js
+++ test/external/userExternal.test.js
@@ -12,9 +12,11 @@ describe('User Endpoints', () => {
 
     describe('POST /users/register', () => {
         it('registrar usuario com sucesso', async () => {
+            let usernameRandom = Math.random().toString(36).substring(7);
+            let passwordRandom = Math.random().toString(36).substring(7);
             const res = await request('http://localhost:3000')
                 .post('/users/register')
-                .send({ username: 'testusuario', password: 'testsenha' });
+                .send({ username: usernameRandom, password: passwordRandom });
             expect(res.status).to.equal(201);
             expect(res.body).to.have.property('success', true);
             
@@ -27,6 +29,22 @@ describe('User Endpoints', () => {
             expect(res.status).to.equal(409);
             expect(res.body).to.have.property('error', 'Usuário já existe');
         });
+
+        it('realizar login com sucesso', async () => {
+            const res = await request('http://localhost:3000')
+                .post('/users/login')
+                .send({ username: 'testusuariocadastrado', password: '12345' });
+            expect(res.status).to.equal(200);
+            expect(res.body).to.have.property('token');
+        });
+        it('falha ao realizar login com credenciais inválidas', async () => {
+            const res = await request('http://localhost:3000')
+                .post('/users/login')
+                .send({ username: 'testusuariocadastrado', password: 'wrongpassword' });
+            expect(res.status).to.equal(401);
+            expect(res.body).to.have.property('error', 'Credenciais inválidas');
+        });
     });
 
 });
+
