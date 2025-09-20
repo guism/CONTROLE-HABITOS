3f60d476aeed4fc3724e836c8a59456cbbe1c401
guilhermesm <guilherme.serrate.maia@gmail.com>
2025-09-20 11:57:43 -0300

    Adicionando ultimos cenarios no external

--- test/external/habitExternal.test.js
+++ test/external/habitExternal.test.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import request from 'supertest';
 
+
 describe('Habit Endpoints', () => {
     let token;
 
@@ -37,6 +38,50 @@ describe('Habit Endpoints', () => {
                 .send({ habitName: 'Novo Hábito' });
             expect(res.status).to.equal(409);
             expect(res.body).to.have.property('error', 'Hábito já cadastrado');
+        });
+    });
+    describe('POST /habits/{habitsName}/complete', () => {
+        it('Completar hábito com sucesso', async () => {
+            let randomHabitName = Math.random().toString(36).substring(7);
+            await request('http://localhost:3000')
+                .post('/habits')
+                .set('Authorization', `Bearer ${token}`)
+                .send({ habitName: randomHabitName });
+             const res = await request('http://localhost:3000')
+                .post('/habits/' + randomHabitName + '/complete')
+                .set('Authorization', `Bearer ${token}`);
+
+            expect(res.status).to.equal(200);
+            expect(res.body.success).to.equal(true);
+        });
+        it('Falha ao completar hábito não existente', async () => {
+            const res = await request('http://localhost:3000')
+                .post('/habits/Hábito Inexistente/complete')
+                .set('Authorization', `Bearer ${token}`);
+            expect(res.status).to.equal(400);
+            expect(res.body).to.have.property('error', 'Hábito não encontrado');
+        });
+    
+    describe('GET /habits/history', () => {
+        // Adicione seus testes aqui ou remova este bloco se não for necessário
+        it('Obter histórico de hábitos concluídos', async () => {
+            await request('http://localhost:3000')
+                .post('/habits')
+                .set('Authorization', `Bearer ${token}`)
+                .send({ habitName: 'Novo Hábito' });
+            await request('http://localhost:3000')
+                .post('/habits/Novo Hábito/complete')
+                .set('Authorization', `Bearer ${token}`);
+            const res = await request('http://localhost:3000')
+                .get('/habits/history')
+                .set('Authorization', `Bearer ${token}`);
+            expect(res.status).to.equal(200);
+            expect(res.body).to.be.an('array');
+            expect(res.body.length).to.be.greaterThan(0);
+            expect(res.body[0]).to.have.property('habitName', 'Novo Hábito');
+            expect(res.body[0]).to.have.property('username', 'testusuariocadastrado');
+        });
+
         });
     });
 });
